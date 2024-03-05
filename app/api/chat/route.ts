d1e4963d1b7a0f6861c8195a254c3338ce925083
Liem Luttrell <liem@epicliem.com>
2024-03-05 12:07:59 -0500

    moved to groq(this thing so quick, and so free)

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,19 @@
 import OpenAI from "openai";
 import { PassThrough} from "stream";
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import Groq from "groq-sdk";
 
 export const runtime = 'edge';
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY,
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_KEY,
+// });
+
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
 });
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const ingredients = searchParams.get('ingredients')
@@ -17,8 +24,8 @@ export async function GET(request: Request) {
 
     console.log("ran")
 
-    const response = await openai.chat.completions.create({
-        model: "gpt-4-turbo-preview",
+    const response = await groq.chat.completions.create({
+        model: "mixtral-8x7b-32768",
         messages: [
             {
                 "role": "system",
@@ -36,7 +43,29 @@ export async function GET(request: Request) {
         presence_penalty: 0,
         stream: true
     });
+
+
+    // const response = await openai.chat.completions.create({
+    //     model: "gpt-4-turbo-preview",
+    //     messages: [
+    //         {
+    //             "role": "system",
+    //             "content": `Help the user with the recipe. Info: \n ingredients:${ingredients} \n instructions:${instructions} \n title:${title} \n servings:${servings}`
+    //         },
+    //         {
+    //             "role": "user",
+    //             "content": message
+    //         }
+    //     ],
+    //     temperature: 1,
+    //     max_tokens: 512,
+    //     top_p: 1,
+    //     frequency_penalty: 0,
+    //     presence_penalty: 0,
+    //     stream: true
+    // });
         const stream =OpenAIStream(response);
+        console.log(`Help the user with the recipe. Info: \n ingredients:${ingredients} \n instructions:${instructions} \n title:${title} \n servings:${servings}`)
 
     return new StreamingTextResponse(stream);
 }
